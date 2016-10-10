093e138312140cf9e14007042e115c1100c65950
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-10-10 19:31:11 +0200

    persistence part.
    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

---
+++ lms-box/lms-box-ejb/src/main/java/tn/esprit/cs/g2/entities/User.java
@@ -0,0 +1,32 @@
+package tn.esprit.cs.g2.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class User implements Serializable {
+	@Id
+	private int id;
+	private String name;
+
+	public User() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
