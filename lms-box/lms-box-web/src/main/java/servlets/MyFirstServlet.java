3bc0016f017504f259d7646c413a43c0467e0c0a
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-11-18 20:21:24 +0100

    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- lms-box/lms-box-web/src/main/java/servlets/MyFirstServlet.java
+++ lms-box/lms-box-web/src/main/java/servlets/MyFirstServlet.java
@@ -2,16 +2,23 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tn.esprit.cs.g2.entities.Course;
+import tn.esprit.cs.g2.services.CourseManagementLocal;
+
 /**
  * Servlet implementation class MyFirstServlet
  */
 public class MyFirstServlet extends HttpServlet {
+	@EJB
+	private CourseManagementLocal courseManagementLocal;
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -29,7 +36,16 @@ public class MyFirstServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter printWriter = response.getWriter();
-		printWriter.write("hello");
+		String nameCourse = request.getParameter("nameC");
+		String idTeacher = request.getParameter("idT");
+
+		Course course = new Course();
+		course.setName(nameCourse);
+		courseManagementLocal.addCourseWithTeacher(course, Integer.parseInt(idTeacher));
+
+		Date date = new Date();
+		printWriter.write("<html><body><h1><font color='red'>hello " + nameCourse + "</font></h1><br>" + "it is = "
+				+ date + "" + "</body></html>");
 
 	}
 
